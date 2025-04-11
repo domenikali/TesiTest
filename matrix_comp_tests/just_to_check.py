e1fd0b2ffdf9ac533573b5ead693e41622e2aa32
DOMENICALI leonardo <leonardo.domenicali@etu.u-paris.fr>
2025-04-11 07:53:22 +0200

    added a python scritp to check the MVM result, it's going to be very slow

--- matrix_comp_tests/just_to_check.py
+++ matrix_comp_tests/just_to_check.py
@@ -1,7 +1,47 @@
 import numpy as np 
 
+#this will be sooo slow ahhh
 
-a = np.array([[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]])
-b = np.array([1,1,1,1])
+def matrix_from_file():
+  f = open("matrix.conf", "r")
 
-print(a.dot(b))
+  line = f.readline()
+  matrix = []
+  while line:
+    row = line.split('-')
+    matrix_row = []
+
+    for i in range(len(row)):
+      wheigts = row[i].split(' ')
+      weight = 1
+      for j in range(len(wheigts)):
+        weight *= int(wheigts[j].replace('\n',''))
+
+      matrix_row.append(weight)
+    matrix.append(matrix_row)
+    line = f.readline()
+
+  f.close()
+  np_matrix = np.array(matrix)
+  return np_matrix
+
+def vector_from_file():
+  f = open("vector.conf", "r")
+
+  line = f.readline()
+  elements = line.split('-')
+  vector = []
+  for i in range(len(elements)):
+    x = int(elements[i].replace('\n',''))
+    vector.append(x)
+
+  f.close()
+  np_vector = np.array(vector)
+  return np_vector
+
+matrix = matrix_from_file() 
+vector = vector_from_file()
+
+print(matrix)
+print(vector)
+print(matrix.dot(vector))
